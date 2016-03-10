2be714dba677512f6762493f07c737fb5304aaa0
Carolyn Sy <csy@Carolyns-MacBook-Pro.local>
2016-03-10 00:21:56 -0800

    part 1

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -8,15 +8,30 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var phraseLabel: UILabel!
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var guessedLabel: UILabel!
+    
+    var phrase: String = ""
+    var partialPhrase: String = ""
+    var incorrectGuessedChars = [String]()
+    var correctGuessedChars = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        self.phrase = hangmanPhrases.getRandomPhrase()
+        
         print(phrase)
+        partialPhrase = initializePhrase(phrase)
+        phraseLabel.text = partialPhrase
+        guessedLabel.text = "None. Guess a letter!"
+        guessTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +39,62 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        if textField.text == "poop" { // needs checks to make sure valid letter, only one letter is entered, letter has not been guessed already
+            print("POOOOOOP")
+        } else {
+            let c: String! = textField.text
+            if guessLetter(c) {
+                correctGuessedChars.append(c)
+            } else {
+                incorrectGuessedChars.append(c)
+                var newString: String = ""
+                for c in incorrectGuessedChars {
+                    newString = newString + " " + c
+                }
+                guessedLabel.text = newString
+            }
+        }
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        phraseLabel.text = self.partialPhrase //change this
+    }
+    
+    func guessLetter(c: String) -> Bool {
+        var newString = ""
+        for index in 0..<(phrase.characters.count) {
+            let i = phrase.startIndex.advancedBy(index)
+            let correctLetter = phrase[i]
+            if c == String(correctLetter) {
+                newString = newString + c
+            } else {
+                print(partialPhrase)
+                newString.append(partialPhrase[i])
+            }
+        }
+        if newString == partialPhrase {
+            return false
+        } else {
+            self.partialPhrase = newString
+            return true
+        }
+    }
+    
+    func initializePhrase(inputPhrase: String) -> String {
+        var outputString: String = ""
+        for c in inputPhrase.characters {
+            if (c == " ") {
+                outputString = outputString + " "
+            } else {
+                outputString = outputString + "-"
+            }
+        }
+        return outputString
+    }
 
     /*
     // MARK: - Navigation
